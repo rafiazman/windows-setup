e7e85f1404af5391385f65add8e078ece7276cfd
Rafi A <>
2023-02-12 20:32:35 +1300

    Update script

---
+++ CustomizeTaskbar.ps1
@@ -0,0 +1,47 @@
+<#
+    Customize Taskbar in Windows 11
+    Sassan Fanai / Jörgen Nilsson
+    Original: https://raw.githubusercontent.com/Ccmexec/PowerShell/master/Customize%20TaskBar%20and%20Start%20Windows%2011/CustomizeTaskbar.ps1
+#>
+
+[string]$RegValueName = "CustomizeTaskbar"
+[string]$FullRegKeyName = "HKLM:\SOFTWARE\ccmexec\" 
+
+# Create registry value if it doesn't exist
+If (!(Test-Path $FullRegKeyName)) {
+    New-Item -Path $FullRegKeyName -type Directory -force 
+}
+
+New-itemproperty $FullRegKeyName -Name $RegValueName -Value "1" -Type STRING -Force
+
+REG LOAD HKLM\Default C:\Users\Default\NTUSER.DAT
+
+Write-Host "Attempting to run: Remove Task View button from Taskbar"
+$reg = New-ItemProperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -Value "0" -PropertyType Dword -Force
+try { $reg.Handle.Close() } catch {}
+
+Write-Host "Attempting to run: Remove Widgets button from Taskbar"
+$reg = New-ItemProperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarDa" -Value "0" -PropertyType Dword -Force
+try { $reg.Handle.Close() } catch {}
+
+Write-Host "Attempting to run: Remove Chat from Taskbar"
+$reg = New-ItemProperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarMn" -Value "0" -PropertyType Dword -Force
+try { $reg.Handle.Close() } catch {}
+
+Write-Host "Attempting to run: Move Start Menu to left side (0 = Left)"
+$reg = New-ItemProperty "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAl" -Value "0" -PropertyType Dword -Force
+try { $reg.Handle.Close() } catch {}
+
+Write-Host "Attempting to run: Remove search from Taskbar"
+$RegKey = "HKLM:\Default\Software\Microsoft\Windows\CurrentVersion\Search"
+if (-not(Test-Path $RegKey )) {
+    $reg = New-Item $RegKey -Force | Out-Null
+    try { $reg.Handle.Close() } catch {}
+}
+$reg = New-ItemProperty $RegKey -Name "SearchboxTaskbarMode"  -Value "0" -PropertyType Dword -Force
+try { $reg.Handle.Close() } catch {}
+
+[GC]::Collect()
+REG UNLOAD HKLM\Default
+
+stop-process -name explorer –force
--- setup.ps1
+++ setup.ps1
@@ -3,9 +3,16 @@
 #
 # Written by Rafi Azman
 
-#Requires -RunAsAdministrator
-
 $base_url = "https://raw.githubusercontent.com/rafiazman/windows-setup/master"
+
+function Test-Administrator {  
+    [OutputType([bool])]
+    param()
+    process {
+        [Security.Principal.WindowsPrincipal]$user = [Security.Principal.WindowsIdentity]::GetCurrent();
+        return $user.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator);
+    }
+}
 
 function Invoke-Remote-Script {
     param (
@@ -22,9 +29,15 @@ function Show-Menu {
     Write-Host "================ $Title ================"
     
     Write-Host "1: Apply registry tweaks"
-    Write-Host "2: Install apps"
+    Write-Host "2: Install apps (requires internet connection)"
     Write-Host "Q: Press 'Q' to quit."
     Write-Host " "
+}
+
+if (-not (Test-Administrator)) {
+    # TODO: define proper exit codes for the given errors 
+    Write-Error "This script must be executed as Administrator.";
+    exit 1;
 }
 
 do {
@@ -33,6 +46,7 @@ do {
     switch ($selection) {
         '1' {
             Invoke-Remote-Script "apply-reg-tweaks.ps1"
+            Invoke-Remote-Script "CustomizeTaskbar.ps1"
         } '2' {
             Invoke-Remote-Script "install-apps.ps1"
         }
